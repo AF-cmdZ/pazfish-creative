4680fde002952bcde4c25af07e82582887d4687a
A Fish <ashley.paz.fish@gmail.com>
2021-09-28 15:36:38 -0500

    hamburger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./topbar.scss";
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
@@ -8,10 +9,21 @@ export default function Topbar() {
                 <div className="left">
                     <a href="#intro" className="logo">paz fish creative</a>
                     <div className="itemContainer">
-                        
+                        <Person className="icon"/>
+                        <span>222-222-2222</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>ashley.paz.fish@me.com</span>
                     </div>
                 </div>
-                <div className="right">This is right</div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>        
+                        <span className="line2"></span>        
+                        <span className="line3"></span>        
+                    </div>
+                </div>
             </div>
         </div>
     )
