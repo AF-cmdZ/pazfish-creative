cbd02802c713368243b5e35ffa5a706d9498e895
A Fish <ashley.paz.fish@gmail.com>
2021-09-28 14:33:36 -0500

    footer and style

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Portfolio from "./components/portfolio/Portfolio.jsx";
 import Works from "./components/works/Works.jsx";
 import Testimonials from "./components/testimonials/Testimonials.jsx";
 import Contact from "./components/contact/Contact.jsx";
+import "./app.scss";
 
 
 function App() {
