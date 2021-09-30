84ff563b491862865c302b9ee1ebe9ae0a9edced
A Fish <ashley.paz.fish@gmail.com>
2021-09-30 14:15:22 -0500

    bio pic and color palette

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,7 +5,9 @@ export default function Intro() {
     return (
         <div className="intro" id="intro">
             <div className="left">
-                
+                <div className="imgContainer">
+                    <img src="assets/AshleyFish_bioPic_web_crop.jpg" alt=""/>
+                </div>
             </div>
             <div className="right"></div>
         </div>
