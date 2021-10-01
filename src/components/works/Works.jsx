21366d78f37fc857860cc9d14ffb0477aba89d14
A Fish <ashley.paz.fish@gmail.com>
2021-10-01 18:36:07 -0500

    works

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import "./works.scss";
+import {PhoneIphone, ArrowBackIos, ArrowForwardIos} from "@material-ui/icons"
 
 export default function Works() {
+
+    const data = [
+        
+    ]
+
     return (
         <div className="works" id="works">
-            Works Section
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <PhoneIphone className="icon-phone"/>
+                                </div>
+                                <h2>Title</h2>
+                                <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia nisi et pulvinar fringilla. 
+                                </p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src="https://source.unsplash.com/random" alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ArrowBackIos className="arrow-left"/>
+            <ArrowForwardIos className="arrow-right"/>
         </div>
-    )
+    );
 }
