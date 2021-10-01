72ab01b11082f66a3b843433a4f702e8ab325af2
A Fish <ashley.paz.fish@gmail.com>
2021-10-01 14:58:38 -0500

    front page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,21 +5,17 @@ import {KeyboardArrowDown} from "@material-ui/icons"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            <div className="left">
-                <div className="imgContainer">
-                    <img src="assets/AshleyFish_bioPic_web_crop.jpg" alt=""/>
-                </div>
-            </div>
-            <div className="right">
                 <div className="wrapper">
+                    <div className="imgContainer">
+                        <img src="assets/AshleyFish_bioPic_web_crop.jpg" alt=""/>
+                    </div>
                     <h3>Ashley Paz Fish</h3>
                     <h1><span>graphic designer</span></h1>
                     <p>I am a seasoned graphic designer and novice web developer. Paired with these complimentary skill sets, I can provide you with unique creative development in print, digital, or web.</p>
                 </div>
-                <a href="#portfolio">
-                    <KeyboardArrowDown/>
+                <a href="#portfolio" className="arrow">
+                    <KeyboardArrowDown className="arrowDown"/>
                 </a>
             </div>
-        </div>
     )
 }
