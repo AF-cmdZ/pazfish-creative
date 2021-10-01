1e98351e3b69d2355f182f7d9bace0e9a0021c8f
A Fish <ashley.paz.fish@gmail.com>
2021-10-01 16:02:36 -0500

    basic portfolio layout

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,20 @@ import "./portfolio.scss";
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            Portfolio Section
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web Applications</li>
+                <li>Design</li>
+                <li>Point of Sale</li>
+            </ul>
+
+            <div className="container">
+                <div className="item">
+                    <img src="https://source.unsplash.com/random" alt="unsplash"/>
+                    <h3>Working Title</h3>
+                </div>
+            </div>
         </div>
     )
 }
