0a59fcc6325a6dc56c85b067e683ae831aca39c6
A Fish <ashley.paz.fish@gmail.com>
2021-10-01 18:59:31 -0500

    working slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import {React, useState} from "react";
 import "./works.scss";
 import {PhoneIphone, ArrowBackIos, ArrowForwardIos} from "@material-ui/icons"
 
 export default function Works() {
 
+    const [currentSlide, setCurrentSlide] = useState(0);
+
     const data = [
-        
-    ]
+        {
+            id: 1,
+            title: "Featured Title 1",
+            img: "https://source.unsplash.com/random",
+            desc: "One Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia nisi et pulvinar fringilla."
+        },
+        {
+            id: 2,
+            title: "Featured Title 2",
+            img: "https://res.cloudinary.com/dxyv6bnjd/image/upload/v1633118767/samples/ecommerce/leather-bag-gray.jpg",
+            desc: "Two Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia nisi et pulvinar fringilla."
+        },
+        {
+            id: 3,
+            title: "Featured Title 3",
+            img: "https://source.unsplash.com/random",
+            desc: "Three Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia nisi et pulvinar fringilla."
+        }
+    ];
+
+    // Slider
+    const handleClick = (way)=>{
+        way === "left-arrow" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) :
+        setCurrentSlide(currentSlide < data.length -1 ? currentSlide + 1 : 0)
+    };
 
     return (
         <div className="works" id="works">
-            <div className="slider">
+            <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)` }}>
+                {data.map(d => (
                 <div className="container">
                     <div className="item">
                         <div className="left">
@@ -18,21 +44,21 @@ export default function Works() {
                                 <div className="imgContainer">
                                     <PhoneIphone className="icon-phone"/>
                                 </div>
-                                <h2>Title</h2>
+                                <h2>{d.title}</h2>
                                 <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia nisi et pulvinar fringilla. 
+                                 {d.desc}
                                 </p>
                                 <span>Projects</span>
                             </div>
                         </div>
                         <div className="right">
-                            <img src="https://source.unsplash.com/random" alt=""/>
-                        </div>
-                    </div>
-                </div>
+                            <img src={d.img} alt=""/>
+                        </div> 
+                    </div> 
+                </div>))}
             </div>
-            <ArrowBackIos className="arrow-left"/>
-            <ArrowForwardIos className="arrow-right"/>
+            <ArrowBackIos className="arrow-left"  onClick={()=>handleClick("arrow-left")} />
+            <ArrowForwardIos className="arrow-right" onClick={()=>handleClick("arrow-right")}/>
         </div>
     );
 }
