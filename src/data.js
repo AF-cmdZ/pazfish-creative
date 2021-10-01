cf87b86087cc92f620532745fa19c2d550ac4dd2
A Fish <ashley.paz.fish@gmail.com>
2021-10-01 16:49:34 -0500

    data file

---
+++ src/data.js
@@ -0,0 +1,71 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Featured Title 1",
+        img: "https://source.unsplash.com/random",
+    },
+    {
+        id: 2,
+        title: "Featured Title 2",
+        img: "https://source.unsplash.com/random",
+    },
+    {
+        id: 3,
+        title: "Featured Title 3",
+        img: "https://source.unsplash.com/random",
+    }
+];
+
+export const webAppPortfolio = [
+    {
+        id: 1,
+        title: "Web App Title 1",
+        img: "https://source.unsplash.com/random",
+    },
+    {
+        id: 2,
+        title: "Web App Title 2",
+        img: "https://source.unsplash.com/random",
+    },
+    {
+        id: 3,
+        title: "Web App Title 3",
+        img: "https://source.unsplash.com/random",
+    }
+];
+
+export const designPortfolio = [
+    {
+        id: 1,
+        title: "Design Title 1",
+        img: "https://source.unsplash.com/random",
+    },
+    {
+        id: 2,
+        title: "Design Title 2",
+        img: "https://source.unsplash.com/random",
+    },
+    {
+        id: 3,
+        title: "Design Title 3",
+        img: "https://source.unsplash.com/random",
+    }
+];
+
+export const posPortfolio = [
+    {
+        id: 1,
+        title: "Portfolio Title 1",
+        img: "https://source.unsplash.com/random",
+    },
+    {
+        id: 2,
+        title: "Portfolio Title 2",
+        img: "https://source.unsplash.com/random",
+    },
+    {
+        id: 3,
+        title: "Portfolio Title 3",
+        img: "https://source.unsplash.com/random",
+    }
+];
