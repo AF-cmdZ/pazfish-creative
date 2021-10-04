12353b238c68065977cd5d7dad21174a24de8b1f
A Fish <ashley.paz.fish@gmail.com>
2021-10-04 12:26:54 -0500

    form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import {React, useState} from "react";
 import "./contact.scss";
-import {ConnectWithoutContact} from "@material-ui/icons"
+import {PersonPin} from "@material-ui/icons"
+
 
 export default function Contact() {
+    const [message, setMessage] = useState(false);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    }
+
     return (
         <div className="contact" id="contact">
             <div className="left">
-                <ConnectWithoutContact/>
+                <PersonPin className="icon"/>
             </div>
             <div className="right">
                 <h2>Contact Me</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks, I will reply ASAP.</span>}
                 </form>
             </div>
         </div>
