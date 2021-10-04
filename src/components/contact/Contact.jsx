d7029639ef6fc1d9daf38f088668aa4197525c00
A Fish <ashley.paz.fish@gmail.com>
2021-10-04 11:19:41 -0500

    contact form start

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./contact.scss";
+import {ConnectWithoutContact} from "@material-ui/icons"
 
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-            Contact Section
+            <div className="left">
+                <ConnectWithoutContact/>
+            </div>
+            <div className="right">
+                <h2>Contact Me</h2>
+                <form>
+                    <input type="text" placeholder="Email" />
+                </form>
+            </div>
         </div>
     )
 }
